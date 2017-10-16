ed9ee4ecb74b81d15f4a8b116f94d1e7504349d4
Meera <mjvsharma@outlook.com>
2017-10-16 12:19:52 -0400

    made a party guests table and then did migrate/rollback/migrate

---
+++ db/migrate/20171016155736_create_party_guests.rb
@@ -0,0 +1,16 @@
+class CreatePartyGuests < ActiveRecord::Migration[5.0]
+  def change
+    create_table :party_guests do |t|
+      t.string "first_name"
+      t.string "last_name"
+      t.string "dietary_restrictions"
+      t.decimal "salary"
+      t.integer "number_of_kids"
+      t.string "vulnerabilities"
+      t.string "illnesses"
+      t.string "medication"
+      t.string "voting preferences"
+      
+    end
+  end
+end
